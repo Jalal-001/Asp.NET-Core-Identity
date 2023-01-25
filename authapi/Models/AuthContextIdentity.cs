275e6910d85690568eff7a2d1dd6da17926bf7ee
Calal.Xaliqov <Calal.Xaliqov@abc-telecom.az>
2023-01-25 17:51:55 +0400

    refactoring

--- authapi/Models/AuthContextIdentity.cs
+++ authapi/Models/AuthContextIdentity.cs
@@ -12,6 +12,6 @@ namespace authapi.Models
         {
             optionsBuilder.UseSqlite("Data Source=AuthDb");
         }
-        public DbSet<AppUser>Users{get;set;}
+        public DbSet<AppUser>Users{get;set;} //buna baxarsan
     }
 }
